3bac7e0268b779b2819cd8216bc48852443ecab6
Christ Schlacta <cschlact@uci.edu>
2014-04-02 14:13:17 +0000

    adding conditional extern C wrappers to C headers

--- P1/COMPARE.h
+++ P1/COMPARE.h
@@ -1,7 +1,15 @@
 #ifndef COMPARE_H
 #define COMPARE_H
-extern "C"{
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 double dshrandom( long );
 int COMPARE(int arg1, int arg2, ...);
+
+#ifdef __cplusplus
 }
+#endif
+
 #endif
--- P1/dshrandom.h
+++ P1/dshrandom.h
@@ -1,5 +1,14 @@
 #ifndef DSHRANDOM_H
 #define DSHRANDOM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 double dshrandom( long input );
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
