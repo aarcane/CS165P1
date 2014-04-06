aa246adb8de293674b613d05b2ef1640dcc62980
Christ Schlacta <cschlact@uci.edu>
2014-04-06 22:41:44 +0000

    simple argument parsing in main.cpp.  updated readme, makefile.

--- P1/README.txt
+++ P1/README.txt
@@ -1,3 +1,9 @@
 got some ideas but no code from the following URL:
 
 http://www.geeksforgeeks.org/k-largestor-smallest-elements-in-an-array/
+
+run as make; make test; for default operation.
+
+run as make; ./doalg n k r [...] where n = size of array, k = num elements to find, and r = number of runs.
+
+./doalg is the same as ./doalg 10 3 1000 100 10 1000 1000 20 1000 10000 40 1000
--- P1/main.cpp
+++ P1/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <climits>
+#include <cstring>
+#include <vector>
 #include "doalg.h"
 class testrun
 {	int datasize;
@@ -29,14 +31,35 @@ public:
 	{	stream << "Testrun: (" << t.datasize << "," << t.ksize << ") " << t.numruns << " runs.  Avg: " << t.avg << " Min: " << t.min << " Max: " << t.max;
 		return stream;
 }	};
-
+std::string usage = ": read README.txt";
 int main (int argc, char** argv)
-{	testrun t[] = {
+{	testrun defaulttests[] = {
 		{15, 3, 1000}, 
 		{100, 10, 1000},
 		{1000, 20, 1000},
 		{10000, 40, 1000} };
-	int numtests = 4;
-	for(int i = 0; i < numtests; ++i) std::cout << *(t[i].run()) << std::endl;
+	int defaultnumtests = 4;
+	std::vector<testrun> t;
+	
+	if(argc == 1)
+	{	for(int i = 0; i < defaultnumtests; ++i) t.push_back(defaulttests[i]);
+	}
+	else if(argc % 3 == 1) 	for( int i = 1; i < argc; i+=3)
+	{	int n, k, r = 1;
+		try
+		{	n = std::stoi(argv[i]);
+			k = std::stoi(argv[i+1]);
+			r = std::stoi(argv[i+2]);
+		}
+		catch(std::exception e)
+		{	std::cerr << "Exception: " << e.what() << std::endl;
+		}
+		t.emplace_back(n, k, r);
+	}
+	else
+	{	std::cerr << argv[0] << usage << std::endl;
+		return -1;
+	}
+	for(std::vector<testrun>::iterator i = t.begin(); i != t.end(); ++i) std::cout << *(i->run()) << std::endl;
 	return 0;
 }
