c7cbff3573d2dd44b0bb251fb01572a7f3d85835
Christ Schlacta <cschlact@uci.edu>
2014-04-07 14:25:53 +0000

    minor local correction

--- P1/doalg.h
+++ P1/doalg.h
@@ -3,6 +3,6 @@
 int doalg(const int n, const int k);
 struct compare; 
 void heapify (const int argc, int * argv);
-void findLargest (const int n, const int k, int * argv, int * Rest);
+void findLargest (const int n, const int k, int * argv);
 void Sort (const int k, int * argv);
 #endif
