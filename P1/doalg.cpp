2a5d0ec52b76dda48e9e550ac6117af184547441
Christ Schlacta <cschlact@uci.edu>
2014-04-07 22:23:48 +0000

    Working in-place heap operations, including percDown and Sort.

--- P1/doalg.cpp
+++ P1/doalg.cpp
@@ -4,13 +4,36 @@
 #include <vector>
 #include <functional>
 #include <iostream>
+// Struct for overloading default comparator in priority_queue
+struct compare
+{
+  bool operator()(const int& l, const int& r)
+  {
+    int compareRet = COMPARE(l, r);
+    //std::cout << "l: " << l << " r: " << r << " c: " << compareRet;
+    if (compareRet == -1)
+    {
+      std::cerr << " x or y out of range c: " << compareRet << std::endl;
+	return false;
+    }
+    else if (compareRet == 2) // r > l
+      {  //std::cout << std::endl;
+      return false;
+    }
+    //std::cout << std::endl;
+    return true; // l > r
+  }
+};
 
+
+// Create priority queue for use as minHeap
+std::priority_queue<int, std::vector<int>, compare > minHeap;
 
 int doalg(const int n, const int k)
 {	
 	int ret = 0;
 	int * Best = new int [k+1]; // stores indices of first k
-        
+        Best[0] = -1;
 	for(int i=1; i<=k;++i) Best[i] = i; //initialize Best to first N values.
         
 	COMPARE(0,n); //allocate hidden array
@@ -20,52 +43,34 @@ int doalg(const int n, const int k)
 	findLargest(n, k, Best);
 	Sort(k, Best);
 	//Cleanup and returns.
-
+	
+	//std::cerr << "heapify(): ";
+	//for(int i = 1; i <=k; ++i) std::cerr << Best[i];
+	//std::cerr << std::endl;
 	ret = COMPARE(-1, k, Best); //better return a positive value.
+
+	for(int i = 1; i <= k; ++i)
+	{	Best[minHeap.size()] = minHeap.top();
+		minHeap.pop();
+	}
+	//std::cerr << "std::priority_queue(): ";
+	//for(int i = 1; i <= k; ++i) std::cerr << Best[i];
+	//std::cerr << std::endl;
+	ret = COMPARE(-1, k, Best);
+
 	delete[] Best;
 	return ret;
 }
-// Struct for overloading default comparator in priority_queue
-struct compare  
-{  
-  bool operator()(const int& l, const int& r)  
-  {  
-    int compareRet = COMPARE(l, r); 
-    //std::cout << "l: " << l << " r: " << r << " c: " << compareRet;
-    if (compareRet == -1)
-    {
-      std::cout << " x or y out of range c: " << compareRet << std::endl;
-    }
-    else if (compareRet == 2) // r > l
-      {  //std::cout << std::endl;
-      return false; 
-    } 
-    //std::cout << std::endl;
-    return true; // l > r
-  }
-};
-
-// Create priority queue for use as minHeap
-std::priority_queue<int, std::vector<int>, compare > minHeap;
 
 // Heapify argv from indeces 1 to argc.
 void heapify (const int argc, int * argv)
 {
-
   // Push elements onto MinHeap
-  for(int i = 1; i <= argc; i ++)
+  for(int i = 1; i <= argc; ++i)
   {
-    minHeap.push(argv[i]);
+    minHeap.push(i);
   }
-  /*
-  std::cout << " MINHEAP ELEMENTS " << std::endl; 
-   while(!minHeap.empty())
-    {
-      std::cout << argv[minHeap.top()] << std::endl;
-      minHeap.pop();
-    }
-  */
-  
+	for(int i = argc; i > 0; --i) percDown(argc, i, argv);
 }
 
 // Iterate through hidden array in COMPARE from k+1 to n and find k largest values.
@@ -84,69 +89,39 @@ void findLargest (const int n, const int k, int * argv)
       
       if(compareRet == -1)//x and y are not in range
       {
-        std::cout << "x or y out of range" << std::endl;
+        std::cerr << "x or y out of range" << std::endl;
       }
       else if(compareRet == 1) // element is greater, make it root
       {
         minHeap.pop();
         minHeap.push(i);
       }
-    }
 
-   for(int i = 1; i <= k; ++i)
-   {
-     argv[minHeap.size()] = minHeap.top();
-     minHeap.pop();
-   }
-   //std::cout << "CHECK BEST" << std::endl; 
-   //std::cout << COMPARE(-1, k, argv) << std::endl; 
-   
-  
+      compareRet = COMPARE(i, argv[1]);
+      if(compareRet == -1) std::cerr << "x or y are out of range" << std::endl;
+      else if(compareRet == 1)  //perc Down
+      {	argv[1] = i;
+	percDown(k, 1, argv);
+      }
+    }
 }
 
 // Perform an in place sort of the items in the given array from position 1 to position k
 void Sort (const int k, int * argv)
-{
-
+{	for(int i = k; i >= 1; --i)
+	{	std::swap(argv[1], argv[i]);
+		percDown(i-1, 1, argv);
+	}
 }
 
-void percDown(const int k, const int n, int * argv)
+void percDown(const int k, int n, int* argv)
 {	int cmp = n*2;
-	int c = 0;
-	if(n > k) return;
-	if(cmp > k) return;
-	if((cmp+1) <= k)
-	{	c = COMPARE(argv[cmp], argv[cmp+1]);
-		if(c == -1) {/* index out of bounds.  why? */}
-		else if (c == 1) /* y is a better cmp */
-		{	cmp += 1;
-		}
-		else if (c == 2) { /* cmp is already best option */}
-		else {/* unspecified error. */}
-	}
-	c = COMPARE(argv[n], argv[cmp]);
-	if(c == -1) {/* index out of bounds.  why? */}
-	else if (c == 1) /* swap n and cmp, recurse */
-	{	std::swap(argv[n], argv[cmp]);
-		percDown(k, cmp, argv);
-	}
-	else if (c == 2) {/* n is already in proper order.*/}
-	else {/* unspecified error. */}
-	return;
-
-}
-/*	COMPARE( x,y ) compares values of array[x] and array[y]	*/
-/*		returns  1 if array[x] > array[y]		*/
-/*		returns  2 if array[y] > array[x]		*/
-/*		returns -1 if x or y is out of range		*/
-
-void percDown2(const int k, int n, int* argv)
-{	int cmp = n*2;
-	while(cmp <= k && n <= k)
-	{	if(cmp+1 <= k) cmp=(1==COMPARE(argv[cmp], argv[cmp+1]))?cmp+1:cmp;
+	while(cmp<=k && n<=k)
+	{	cmp=((1+cmp)<=k && 1==COMPARE(argv[cmp], argv[(1+cmp)]))?(1+cmp):cmp;
 		if(1==COMPARE(argv[n], argv[cmp]))
 		{	std::swap(argv[n], argv[cmp]);
 			n = cmp; // recurse, if applicable.
+			cmp *= 2;
 		}
 		else return; // actually done.  Don't recurse.
 	}
